596d2ae0722575312c3cc79282f88ab2bd34f72c
DZinchenko <zindan466@gmail.com>
2024-07-22 22:37:46 +0300

    Initial Commit

---
+++ ecoflow_monitor/ecoflow_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoflow_monitorApp.swift
+//  ecoflow_monitor
+//
+//  Created by Zinchenko Danulo on 22.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ecoflow_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
